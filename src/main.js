f23f7d32b2ddd09e5a0095ada41a67aba6496b3f
Max-munene <munenemaxwell2@gmail.com>
2023-01-19 05:11:36 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from ".store";
+import Vue2Editor from "vue2-editor";
+import Vue from "vue";
+import vueConfig from "vue.config";
 
-createApp(App).mount('#app')
+Vue.useAttrs(Vue2Editor);
+
+vueConfig.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
+
+// createApp(App).mount("#app");
